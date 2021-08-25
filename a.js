607324ceb8c0836b9da65dd8adb6f5cfdb4676bb
Your Name <you@example.com>
2021-08-24 22:21:31 -0300

    clase 4

--- a.js
+++ a.js
@@ -5,17 +5,20 @@ var resta_1 = require("./resta");
 var operacion = function (num, numDos, calculo) {
     return new Promise(function (resolve, reject) {
         if (calculo === "+") {
-            resolve(console.log(new suma_1.suma(num, numDos).resultado()));
+            resolve(new suma_1.suma(num, numDos).resultado());
         }
         else if (calculo === "-") {
-            resolve(console.log(new resta_1.resta(num, numDos).resultado()));
+            resolve(new resta_1.resta(num, numDos).resultado());
+        }
+        else {
+            reject("error no es suma o resta");
         }
     });
 };
 var operaciones = function (f, num, numberDos, calculo) {
-    f(num, numberDos, calculo);
+    f(num, numberDos, calculo).then(function (resultado) { return console.log(resultado); })["catch"](function (error) { return console.log(error); });
 };
 console.log('iniciando...');
 console.log("Estamos procesando la operacion");
-operaciones(operacion, 20, 20, "+");
+operaciones(operacion, 20, 20, "-");
 console.log('finalizado');
--- a.ts
+++ a.ts
@@ -5,22 +5,25 @@ import {resta} from "./resta";
 const operacion=(num:number,numDos:number,calculo:string)=>{
   return new Promise ((resolve, reject)=>{
       if(calculo==="+"){
-        resolve(console.log(new suma(num,numDos).resultado()));
+        resolve(new suma(num,numDos).resultado());
          }else if(calculo==="-"){
-          resolve(console.log(new resta(num,numDos).resultado()));
+          resolve(new resta(num,numDos).resultado());
+         }else{
+           reject("error no es suma o resta")
          }
  
   });
 }
 
 const operaciones=(f:(c:number,d:number,calculo:string)=>any,num:number,numberDos:number,calculo:string)=>{
-f(num,numberDos,calculo);
+f(num,numberDos,calculo).then(resultado => console.log( resultado))
+.catch(error => console.log(error));
 
 }
 
   console.log('iniciando...');
   console.log("Estamos procesando la operacion");
-  operaciones(operacion,20,20,"+")
+  operaciones(operacion,20,20,"-")
   console.log('finalizado');
 
 
