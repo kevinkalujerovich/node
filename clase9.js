2061d9ab6c4dc4210334fbe544f361f96b35ec89
Your Name <you@example.com>
2021-09-05 17:54:46 -0300

    actualizacion

--- clase9.js
+++ clase9.js
@@ -1,29 +1,47 @@
 import express from "express";
+import Guardar from "./guardarform.js";
 
 const app = express();
 const PORT = 8080;
 const router = express.Router();
 
+const productos = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/productos", router);
+
+app.use(express.static("public"));
 
 const server = app.listen(PORT, () => {
   console.log("Servidor HTTP escuchando en el puerto", server.address().port);
 });
 server.on("error", (error) => console.log("Error en servidor", error));
 
-router.get("/actualizar/:id", (req, res) => {
-  const objRes = "actualizar";
-  let params = req.params;
-  res.json(objRes + params.id);
+router.get("/listar", (req, res) => {
+  productos.length === 0
+    ? res.status(404).json({ error: "no hay productos cargados" })
+    : res.json(productos);
 });
 
-router.delete("/borrar/:id", (req, res) => {
-  const objRes = "borrar";
-  let params = req.params;
-  res.json(objRes + params.id);
+router.post("/guardar", (req, res) => {
+  let body = req.body;
+  const producto = new Guardar(body.title, body.price, productos.length + 1);
+  productos.push(producto);
+  res.json({ operacion: "Objeto guardado" });
 });
-router.get("/borrar/:id", (req, res) => {
-  const objRes = "borrar";
+
+router.get("/listar/:id", (req, res) => {
   let params = req.params;
-  res.json(objRes + params.id);
+  const found = productos.find((element) => element.id === parseInt(params.id));
+  !found
+    ? res.status(404).json({ error: "producto no encontrado" })
+    : res.json(found);
+});
+
+router.delete("/borrar", (req, res) => {
+  let params = req.params;
+  res.json(params);
+  console.log("asdasdasda");
 });
---
+++ guardarform.js
@@ -0,0 +1,5 @@
+export default class Guardar {
+  constructor(title, price, id) {
+    (this.title = title), (this.price = price), (this.id = id);
+  }
+}
