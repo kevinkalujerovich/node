b66590b9e382f9bd4b1e0c00e465a111140deeaa
Your Name <you@example.com>
2021-09-22 03:02:03 -0300

    actualizar

--- clase 12/public/index.js
+++ clase 12/public/index.js
@@ -1,25 +1,35 @@
 const socket = io();
 
-socket.on('atodos', (data) => {
-    let ul = document.getElementsByTagName('ul')[0];
-    ul.innerHTML = '';
-    for (mensaje of data) {
-        let ul = document.getElementsByTagName('ul')[0];
-        let li = document.createElement('li');
-        ul.appendChild(li);
-        li.innerHTML = `SocketId: ${mensaje.socketId} - Mensaje: ${mensaje.mensaje}`;
-    }
-})
+socket.on("envioProductos", (data) => {
+  if (data.length > 0) {
+    let html = data
+      .map(
+        (e, i) => `
+  <div class="row pt-2 pb-2" >
+    <div class="col-sm pt-2" style=" border-top: 1px solid white">
+     ${e.name}
+    </div>
+    <div class="col-sm pt-2" style=" border-top: 1px solid white" >
+    ${e.price}
+    </div>
+    <div class="col-sm pt-2" style=" border-top: 1px solid white" >
+        <img width="50px" height="50px" src=  ${e.thumbnail}>
+    </div>
+  </div>
+`
+      )
+      .join(" ");
+    document.getElementById("productos").innerHTML = html;
+  } else {
+    document.getElementById("productos").innerHTML = "No hay productos";
+  }
+});
 
-socket.on('devuelvo', (data) => {
-    let p = document.getElementsByTagName('p')[0];
-    p.innerHTML = data;
-})
-
-function enviar() {
-    socket.emit('notificacion', document.getElementsByTagName('input')[0].value);
-};
-
-function cambio(valor) {
-    socket.emit('mitexto', valor);
+function enviarMensaje() {
+  let envio = {
+    name: document.getElementById("name").value,
+    price: document.getElementById("price").value,
+    thumbnail: document.getElementById("thumbnail").value,
+  };
+  socket.emit("envioProducto", envio);
 }
